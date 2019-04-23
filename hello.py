5fa5d5b022d918964f02ea43e3d1406ca9580722
impium <brendondelloma@gmail.com>
2019-04-23 19:14:58 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,54 @@
-from flask import Flask
-app = Flask(__name__)
+import smbus, time, socket, sys
 
-@app.route("/")
+host = '0.0.0.0'
+port = 1500
+socksize = 1024
+s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('',port))
+s.listen(1)
+while True:
+    try:
+        conn, adds = s.accept()
+        data = conn.recv(socksize)
+        
+        bus = smbus.SMBus(1)
+##set register for bus
+        addr = 0x40
+        
+##pick servo
 
-def hello():
-    
-        test = int(input("Command"))
-        if test == 1:
-        
-        
-            return start()
-        elif test == 2:
-            return stop()
+        if data == b'1':
+    ##first servo run
+            bus.write_byte_data(addr,0,0x20)
+            time.sleep(.25)
+            bus.write_byte_data(addr,0x06,0)
+
+            time.sleep(.25)
+##bus.write_byte_data(addr,0xfe, 0x1e)
+            bus.write_byte_data(addr,0, 0x20)
+    ##bus.write_word_data(addr,0x08,1250)
+
+            time.sleep(2)
+            bus.write_word_data(addr,0x08,1600)
+            time.sleep(2)
+            bus.write_word_data(addr, 0x08, 1664)
+
+        elif data == b'killsrv':
+            conn.close()
+            sys.exit()
         else:
-            print("false")
-            return "no"
-if __name__ == "__main__":
-    app.run()
+            bus.write_byte_data(addr,0,0x20)
+            time.sleep(.25)
 
-def start():
-    return "started"
-def stop():
-    return "stop";
+            bus.write_byte_data(addr,0x0A,0)
+
+            time.sleep(.25)
+##bus.write_byte_data(addr,0xfe, 0x1e)
+            bus.write_byte_data(addr,0, 0x20)
+    ##bus.write_word_data(addr,0x0C,1250)
+
+            time.sleep(2)
+            bus.write_word_data(addr,0x0C,500)
+            time.sleep(2)
+            bus.write_word_data(addr, 0x0C, 2000)
+    except socket.timeout:print("")
